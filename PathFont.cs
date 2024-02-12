fc019c87b409946ebc29afe6f034334c7bf6ec1e
Весьмой Шум <68204631+Wholemy@users.noreply.github.com>
2024-02-12 10:18:13 +0300

    2024.0212.1011

--- PathFont.cs
+++ PathFont.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Wholemy {
 	#region #class# PathFont 
 	public abstract class PathFont {
@@ -48,6 +42,21 @@ namespace Wholemy {
 				Width += (Chr.Width * Mul);
 			}
 			return PathSource.CombineToGeometry();
+		}
+		public void ToPath(PathSource P,double X,double Y, string Value, double Height, double Weight) {
+			var Count = Value.Length;
+			var Width = X;
+			var PathSource = P;
+			for (int Index = 0; Index < Count; Index++) {
+				var Char = Value[Index];
+				var Chr = GetChr(Char);
+				var Mul = Height / Chr.Height;
+				PathSource.AddResizeMovA(WH: Mul, X: Width, Y:Y);
+				PathSource.Begin();
+				PathSource.Parse(Chr.GetWeight(Weight));
+				PathSource.CutResize();
+				Width += (Chr.Width * Mul);
+			}
 		}
 		public double GetWidth(string Value, double Height) {
 			var Count = Value.Length;
@@ -106,9 +115,9 @@ namespace Wholemy {
 			public string GetWeight(double Weight) {
 				var S = Map.GetV(ref CacheMap, Weight);
 				if (S == null) {
-					var L = Get(D: Weight / 4).Last;
+					var L = Get(D: Weight / 4).Geometry;
 					if (L != null) {
-						S = Get(D: Weight / 4).Last.ToString();
+						S = Get(D: Weight / 4).Geometry.ToString();
 					} else { S = ""; }
 					Map.Add(ref CacheMap, Weight, S);
 				}
@@ -130,84 +139,84 @@ namespace Wholemy {
 					//S.AddRotate(45, 500, 500);
 					//S.AddResizeMovA(XY: 100);
 					S.AddArc00(50, 200, 200, 50);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddArc00(600, 50, 750, 200);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddArc00(750, 600, 600, 750);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddArc00(200, 750, 50, 600);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddLin00(200, 50, 600, 50);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddLin00(750, 200, 750, 600);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddLin00(600, 750, 200, 750);
-					S.UnionFz();
+					S.BeginFz();
 					S.AddLin00(50, 600, 50, 200);
 					//S.Thickness = 150 - D;
 					var chr = (int)(ushort)this.Str[0];
 					if (((chr >> 15) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200, 200, 25);
 					}
 					if (((chr >> 14) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200 + (400.0 / 3 * 1), 200, 25);
 					}
 					if (((chr >> 13) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600 - (400.0 / 3 * 1), 200, 25);
 					}
 					if (((chr >> 12) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600, 200, 25);
 					}
 					if (((chr >> 11) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200, 200 + (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 10) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200 + (400.0 / 3 * 1), 200 + (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 9) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600 - (400.0 / 3 * 1), 200 + (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 8) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600, 200 + (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 7) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200, 600 - (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 6) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200 + (400.0 / 3 * 1), 600 - (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 5) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600 - (400.0 / 3 * 1), 600 - (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 4) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600, 600 - (400.0 / 3 * 1), 25);
 					}
 					if (((chr >> 3) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200, 600, 25);
 					}
 					if (((chr >> 4) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(200 + (400.0 / 3 * 1), 600, 25);
 					}
 					if (((chr >> 1) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600 - (400.0 / 3 * 1), 600, 25);
 					}
 					if (((chr) & 1) == 1) {
-						S.UnionFz();
+						S.BeginFz();
 						S.AddOrc(600, 600, 25);
 					}
 					//S.CutResize();
